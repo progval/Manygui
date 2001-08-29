58b9d624e440ef29f18d117b51482a5e8027e7f4
mlh <mlh>
2001-08-29 21:19:13 +0000

    Changed ListModel to use add_hint

--- lib/anygui/Models.py
+++ lib/anygui/Models.py
@@ -1,6 +1,8 @@
 from anygui.Mixins import Attrib, Observable
 from UserList import UserList
 
+# FIXME: Selection should probably be a widget property
+
 class ListModel(Attrib, Observable, UserList):
 
     def __init__(self, *arg, **kw):
@@ -18,53 +20,66 @@ class ListModel(Attrib, Observable, UserList):
 
     def __setitem__(self, i, item):
         UserList.__setitem__(self, i, item)
-        self.notify_views(target=self, change=[('__setitem__', (i, item), {})])
+        self.add_hint('__setitem__', i, item)
+        self.notify_views()
 
     def __delitem__(self, i):
         UserList.__delitem__(self, i)
-        self.notify_views(target=self, change=[('__delitem__', (i,), {})])
+        self.add_hint('__delitem__', i)
+        self.notify_views()
 
     def __setslice__(self, i, j, other):
         UserList.__setslice__(self, i, j, other)
-        self.notify_views(target=self, change=[('__setslice__', (i, j, other), {})])
+        self.add_hint('__setslice__', i, j, other)
+        self.notify_views()
         
     def __delslice__(self, i, j):
         UserList.__delslice__(self, i, j)
-        self.notify_views(target=self, change=[('__delslice__', (i, j), {})])
+        self.add_hint('__delslice__', i, j)
+        self.notify_views()
         
     def __iadd__(self, other):
         UserList.__iadd__(self, other)
-        self.notify_views(target=self, change=[('__iadd__', (other,), {})])
+        self.add_hint('__iadd__', other)
+        self.notify_views()
         
     def __imul__(self, n):
         UserList.__imul__(self, n)
-        self.notify_views(target=self, change=[('__imul__', (n,), {})])
+        self.add_hint('__imul__', n)
+        self.notify_views()
         
     def append(self, item):
         UserList.__append__(self, item)
-        self.notify_views(target=self, change=[('append', (item,), {})])
+        self.add_hint('__append__', item)
+        self.notify_views()
     
     def insert(self, i, item):
         UserList.insert(self, i, item)
-        self.notify_views(target=self, change=[('insert', (i, item), {})])
+        self.add_hint('insert', i, item)
+        self.notify_views()
         
     def pop(self, i=-1):
         result = UserList.pop(self, i)
-        self.notify_views(target=self, change=[('pop', (i,), {})])
+        self.add_hint('pop', i)
+        self.notify_views()
         return result
     
     def remove(self, item):
         UserList.remove(self, item)
-        self.notify_views(target=self, change=[('remove', (item,), {})])
+        self.add_hint('remove', item)
+        self.notify_views()
         
     def reverse(self):
         UserList.reverse(self)
-        self.notify_views(target=self, change=[('reverse', ()), {})])
+        self.add_hint('reverse')
+        self.notify_views()
         
     def sort(self, *args):
         UserList.sort(self, *args)
-        self.notify_views(target=self, change=[('sort', args, {})])
+        self.add_hint('sort')
+        self.notify_views()
         
     def extend(self, other):
         UserList.extend(self, other)
-        self.notify_views(target=self, change=[('extend', (other,), {})])
+        self.add_hint('extend', other)
+        self.notify_views()
