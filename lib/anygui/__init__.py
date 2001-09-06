051572b8353203211c1296118106166c764f5ab3
theller <theller>
2001-09-06 15:16:01 +0000

    Insert msw into the list of backends.

--- lib/anygui/__init__.py
+++ lib/anygui/__init__.py
@@ -1,4 +1,4 @@
-_backends = 'x java wx tk beos'
+_backends = 'msw x java wx tk beos'
 
 # Try to get the environment variables ANYGUI_WISHLIST (used
 # before anygui.wishlist), and ANYGUI_DEBUG (to print out
