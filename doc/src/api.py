d3c062888df17f3c498eacb52c1b4b2dcddc3de4
mlh <mlh>
2001-10-21 19:20:42 +0000

    Beginnings of an API reference

---
+++ doc/src/api.py
@@ -0,0 +1,56 @@
+
+# Eventually meant to be used in the API reference...
+
+class Label:
+    ""
+
+class ListBox:
+    ""
+    
+class Button:
+    ""
+    
+class ToggleButtonMixin:
+    ""
+
+class CheckBox:
+    ""
+
+class RadioButton:
+    ""
+
+class RadioGroup:
+    ""
+
+class TextField:
+    ""
+    
+class TextArea:
+    ""
+
+class Window:
+    ""
+
+class Application:
+    ""
+
+class BooleanModel:
+    ""
+
+class ListModel:
+    ""
+
+class TextModel:
+    ""
+
+def application():
+    ""
+
+def register(src, sig, slot):
+    ""
+
+def unregister(src, sig, slot): # deregister() ?
+    ""
+
+def signal(src, sig, kw):
+    ""
