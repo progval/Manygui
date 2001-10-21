f01e524c50579f2049449bebf3917986691111d2
mlh <mlh>
2001-10-21 00:21:30 +0000

    *** empty log message ***

--- lib/anygui/Defaults.py
+++ lib/anygui/Defaults.py
@@ -1,118 +1,118 @@
 "Default values for the anygui package."
 
-# Buttons
+class Button:
 
-button_text = 'Button'
-button_x = 0
-button_y = 0
-button_width = 100
-button_height = 100
-button_hmove = 0
-button_vmove = 0
-button_hstretch = 0
-button_vstretch = 0
-button_visible = 1
-button_enabled = 0
+    _text = 'Button'
+    _x = 0
+    _y = 0
+    _width = 100
+    _height = 100
+    _hmove = 0
+    _vmove = 0
+    _hstretch = 0
+    _vstretch = 0
+    _visible = 1
+    _enabled = 0
 
-# CheckBoxes
+class CheckBox:
 
-checkbox_text = 'CheckBox'
-checkbox_x = 0
-checkbox_y = 0
-checkbox_width = 100
-checkbox_height = 100
-checkbox_hmove = 0
-checkbox_vmove = 0
-checkbox_hstretch = 0
-checkbox_vstretch = 0
-checkbox_visible = 1
-checkbox_enabled = 0
+    _text = 'CheckBox'
+    _x = 0
+    _y = 0
+    _width = 100
+    _height = 100
+    _hmove = 0
+    _vmove = 0
+    _hstretch = 0
+    _vstretch = 0
+    _visible = 1
+    _enabled = 0
 
-# Labels
+class Label:
 
-label_text = 'Label'
-label_x = 0
-label_y = 0
-label_width = 100
-label_height = 100
-label_hmove = 0
-label_vmove = 0
-label_hstretch = 0
-label_vstretch = 0
-label_visible = 1
-label_enabled = 0
+    _text = 'Label'
+    _x = 0
+    _y = 0
+    _width = 100
+    _height = 100
+    _hmove = 0
+    _vmove = 0
+    _hstretch = 0
+    _vstretch = 0
+    _visible = 1
+    _enabled = 0
 
-# ListBoxes
+class ListBox:
 
-listbox_text = 'ListBox'
-listbox_x = 0
-listbox_y = 0
-listbox_width = 100
-listbox_height = 100
-listbox_hmove = 0
-listbox_vmove = 0
-listbox_hstretch = 0
-listbox_vstretch = 0
-listbox_visible = 1
-listbox_enabled = 0
+    _text = 'ListBox'
+    _x = 0
+    _y = 0
+    _width = 100
+    _height = 100
+    _hmove = 0
+    _vmove = 0
+    _hstretch = 0
+    _vstretch = 0
+    _visible = 1
+    _enabled = 0
 
-# RadioButtons
+class RadioButton:
+    
+    _text = 'RadioButton'
+    _x = 0
+    _y = 0
+    _width = 100
+    _height = 100
+    _hmove = 0
+    _vmove = 0
+    _hstretch = 0
+    _vstretch = 0
+    _visible = 1
+    _enabled = 0
 
-radiobutton_text = 'RadioButton'
-radiobutton_x = 0
-radiobutton_y = 0
-radiobutton_width = 100
-radiobutton_height = 100
-radiobutton_hmove = 0
-radiobutton_vmove = 0
-radiobutton_hstretch = 0
-radiobutton_vstretch = 0
-radiobutton_visible = 1
-radiobutton_enabled = 0
+class TextArea:
 
-# TextAreas
+    _text = 'TextArea'
+    _x = 0
+    _y = 0
+    _width = 100
+    _height = 100
+    _hmove = 0
+    _vmove = 0
+    _hstretch = 0
+    _vstretch = 0
+    _visible = 1
+    _enabled = 0
+    _editable = 1
+    _selection = (0, 0)
 
-textarea_text = 'TextArea'
-textarea_x = 0
-textarea_y = 0
-textarea_width = 100
-textarea_height = 100
-textarea_hmove = 0
-textarea_vmove = 0
-textarea_hstretch = 0
-textarea_vstretch = 0
-textarea_visible = 1
-textarea_enabled = 0
-textarea_editable = 1
-textarea_selection = (0, 0)
+class TextField:
 
-# TextFields
+    _text = 'TextField'
+    _x = 0
+    _y = 0
+    _width = 100
+    _height = 100
+    _hmove = 0
+    _vmove = 0
+    _hstretch = 0
+    _vstretch = 0
+    _visible = 1
+    _enabled = 0
+    _editable = 1
+    _selection = (0, 0)
 
-textfield_text = 'TextField'
-textfield_x = 0
-textfield_y = 0
-textfield_width = 100
-textfield_height = 100
-textfield_hmove = 0
-textfield_vmove = 0
-textfield_hstretch = 0
-textfield_vstretch = 0
-textfield_visible = 1
-textfield_enabled = 0
-textfield_editable = 1
-textfield_selection = (0, 0)
+class Window:
 
-# Windows
-
-window_text = 'Window'
-window_x = 0
-window_y = 0
-window_width = 100
-window_height = 100
-window_hmove = 0
-window_vmove = 0
-window_hstretch = 0
-window_vstretch = 0
-window_visible = 1
-window_enabled = 0
-window_title = 'Untitled'
+    _text = 'Window'
+    _x = 0
+    _y = 0
+    _width = 100
+    _height = 100
+    _hmove = 0
+    _vmove = 0
+    _hstretch = 0
+    _vstretch = 0
+    _visible = 1
+    _enabled = 0
+    _title = 'Untitled'
