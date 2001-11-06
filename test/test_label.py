cde60deb0acbe6bdc0df56fa3f205f896ac15496
schinckel <schinckel>
2001-11-06 07:40:21 +0000

    Changed test_label to only use single line labels.

--- test/test_label.py
+++ test/test_label.py
@@ -3,7 +3,10 @@ from anygui import Window, Label, Application
 app = Application()
 
 win = Window(width = 160, height = 100)
-lbl = Label(x = 30, y = 30, height = 32, text = "Spam!\nGlorious Spam!")
-win.add(lbl)
+lb1 = Label(x = 30, y = 30, height = 32, text = "Spam!")
+lb2 = Label(x = 30, y = 48, height = 32, text = "Glorious Spam!")
+
+win.add(lb1)
+win.add(lb2)
 
 app.run()
