a66f6cb1e054feb0c8ad7ead3e16f437cd96b3d5
theller <theller>
2001-11-08 20:30:33 +0000

    Pressing the "Hello" button now create a new button every time. Neither useful nor nice, but...

--- test/test_button.py
+++ test/test_button.py
@@ -2,10 +2,16 @@ from anygui import Window, Button, Application, Options
 
 app = Application()
 
+g_y = 150
+
+win = Window(width = 110, height = 210)
+
 def say_hello():
     print "Hello, world!"
-
-win = Window(width = 110, height = 150)
+    global g_y
+    btn = Button(opt, y = g_y, text="New")
+    g_y += 5
+    win.add(btn)
 
 opt = Options(x = 30, width = 50, height = 30, action = say_hello)
 btn = Button(opt, y = 30, text = "Hello")
