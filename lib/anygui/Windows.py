02c6cebd03b05ee1813580f9fc18ee8071b8b9ab
mlh <mlh>
2001-11-10 14:43:47 +0000

    *** empty log message ***

--- lib/anygui/Windows.py
+++ lib/anygui/Windows.py
@@ -2,31 +2,46 @@ from anygui import application
 from anygui.Frames import AbstractFrame
 from anygui.Exceptions import UnimplementedMethod
 from anygui import Defaults
+import inspect
 
 class AbstractWindow(AbstractFrame, Defaults.Window):
 
+    class CreationSentinel:
+        'Calls win.ensure_created() when leaving local scope'
+        def __init__(self, win):
+            self.win = win
+        def __del__(self):
+            self.win.ensure_created()
+
     def __init__(self, *args, **kw):
-        #self.visible = 0
         AbstractFrame.__init__(self, *args, **kw)
 
-        # Window tiling code:
+        # Window staggering code:
+        # FIXME: Should be offset from current top window, if any
         self._x = Defaults.Window._x
         self._y = Defaults.Window._y
         Defaults.shift_window()
         
         application()._add_window(self)
+
+        # If we are inside a function call, set up a CreationSentinel
+        # in surrounding local scope -- otherwise, just call
+        # ensure_created:                               (mlh20011109)
+        stack = inspect.stack()
+        # FIXME: Find out if we are in the global scope...
+        if len(stack) > 2: # ... > 1...
+            #import pprint
+            #print pprint.pprint(stack)
+            scope = stack[1][0].f_locals
+            name = 0
+            while scope.has_key(`name`): name += 1
+            scope[`name`] = self.CreationSentinel(self)
+        else:
+            self.ensure_created()
         
     def destroy(self):
         self._ensure_destroyed()
         application()._remove_window(self)
-
-    #def show(self):
-    #    "Alias for window.visible = 1. Needed?"
-    #    self._set_visible(1)
-
-    #def hide(self):
-    #    "Alias for window.visible = 0. Needed?"
-    #    self._set_visible(0)
 
     def _set_title(self, text):
         if self._title != text:
--- test/test_window.py
+++ test/test_window.py
@@ -5,18 +5,37 @@ class MyWindow(Window):
 
 app = Application()
 
-win = MyWindow(title = "A Standard Window",
-	     width = 300, height = 200)
+win = MyWindow(title="A Standard Window",
+               width=220, height = 45)
+
+def new_window():
+    # This should not appear until the function returns:
+    win = Window(title='Yay, another window!')
+
+    # Test for flashing (should not be visible):
+    for i in range(5):
+        win.visible = not win.visible
+    win.visible = 1
+
+    #win.ensure_created() # ... Debug...
+    
+    print application()._windows
+    print win
+
+
+btn = Button(text='Create new window', size=(200,25), x=10, y=10,
+             action=new_window)
+win.add(btn)
 
 print application()._windows
 print win
 print
 
-dlog = Window(title = "A Dialog Window", style = 'dialog',
-	      x = 100, y = 300, width = 300, height = 200)
+#dlog = Window(title = "A Dialog Window", style = 'dialog',
+#	      x = 100, y = 300, width = 300, height = 200)
 
-print application()._windows
+#print application()._windows
 
-application().run()
+app.run()
 
 print application()._windows
