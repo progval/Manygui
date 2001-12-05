5f735d2e35af27608348922537c80dad4a29a2d2
mlh <mlh>
2001-12-05 20:12:56 +0000

    *** empty log message ***

--- lib/anygui/Canvases.py
+++ lib/anygui/Canvases.py
@@ -1,48 +1,12 @@
 
-# Not up-to-date
-
 from Components import AbstractComponent
 from Colors import *
+from Fonts import *
 import Defaults
 
 figureLine = 1
 figureArc = 2
 figureCurve = 3
-
-# FIXME: Change this?:
-class Font:
-    "This class represents font typeface, size, and style."
-	
-    def __init__(self, size=12, bold=0, italic=0, underline=0, face=None):
-        # public mode variables
-        d = self.__dict__
-        d["bold"] = bold
-        d["italic"] = italic
-        d["underline"] = underline
-
-        # public font size (points)
-        d["size"] = size
-
-        # typeface -- a name or set of names, interpreted by the Canvas,
-        # or "None" to indicate the Canvas-specific default typeface
-        d["face"] = face
-	
-    def __cmp__(self, other):
-        """Compare two fonts to see if they're the same."""
-        if self.face == other.face and self.size == other.size and \
-           self.bold == other.bold and self.italic == other.italic \
-           and self.underline == other.underline:
-            return 0
-        else:
-            return 1
-
-    def __repr__(self):
-        return "Font(%d,%d,%d,%d,%s)" % (self.size, self.bold, self.italic, \
-                                         self.underline, repr(self.face))
-
-    def __setattr__(self, name, value):
-        raise TypeError, "piddle.Font has read-only attributes"
-
 
 class AbstractCanvas(AbstractComponent, Defaults.Canvas):
 
@@ -180,7 +144,7 @@ class AbstractCanvas(AbstractComponent, Defaults.Canvas):
 
     def drawLine(self, x1, y1, x2, y2, color=None, width=None):
         "Draw a straight line between x1,y1 and x2,y2."
-        self.drawPolygon([(x1,y1), (x2, y2)],
+        self.drawPolygon([(x1, y1), (x2, y2)],
                          edgeColor=color,
                          edgeWidth=width)
 
