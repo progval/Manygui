e6e4bf810387a5d9dc61dd653e1dcb1f78f86751
mlh <mlh>
2001-12-08 23:25:53 +0000

    *** empty log message ***

---
+++ doc/src/insertdate.py
@@ -0,0 +1,19 @@
+''' Replace contents of a file between <date> and </date>
+    with the current date. Assumes that both tags are on
+    the same line.
+'''
+
+import fileinput, re, time
+
+endings = ['st', 'nd', 'rd'] + 17 * ['th'] \
+        + ['st', 'nd', 'rd'] +  7 * ['th'] \
+        + ['st']
+
+for line in fileinput.input():
+    format = '<date>%B %%i%%s, %Y</date>'
+    datestring = time.strftime(format)
+    day = time.localtime()[2]
+    ending = endings[day]
+    datestring %= (day, ending)
+    pattern = '<date>.*?</date>'
+    print re.sub(pattern, datestring, line),
