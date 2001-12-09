58c56abaef50450507fbd25ed528fe3b53d624ab
mlh <mlh>
2001-12-09 23:39:09 +0000

    *** empty log message ***

--- test/test_button.py
+++ test/test_button.py
@@ -1,27 +1,26 @@
 from anygui import *
 
-app = Application()
-
 g_y = 150
 
-win = Window(width = 200, height = 300)
-app.add(win)
-
 def say_hello(**kw):
-    #print "Hello, world!"
     global g_y
     #btn = Button(opt, y = g_y, text="New")
-    btn = Button(geometry = (30,g_y,50,30))
+    btn = Button(geometry = (30,g_y,50,30), text="New")
     g_y += 5
     win.add(btn)
 
 opt = Options(x = 30, width = 50, height = 30)
+
 btn = Button(opt, y = 30, text = "Hello")
 link(btn, say_hello)
+
 dis = Button(opt, y = 90, text = "Goodbye", enabled = 0)
 link(dis, say_hello)
 
+win = Window(width = 200, height = 300)
 win.add(btn)
 win.add(dis)
 
+app = Application()
+app.add(win)
 app.run()
