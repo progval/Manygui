7607ae589aa9383b5657ab4b7f308b76c6eb17f4
mlh <mlh>
2001-12-09 23:15:51 +0000

    *** empty log message ***

--- lib/anygui/Applications.py
+++ lib/anygui/Applications.py
@@ -1,12 +1,26 @@
 #from Mixins import Action
 from Exceptions import UnimplementedMethod
+from Mixins import Attrib
 import anygui
 
-class AbstractApplication:#(Action): # FIXME: Is Action needed here?
+class AbstractApplication(Attrib):#(Action): # FIXME: Is Action needed here?
     
     def __init__(self):
         self._windows = []
         anygui._application = self
+
+    # FIXME: Move functionality from _add_window and _remove_window to
+    # add/remove, and from windows() to _get_contents when the rest of
+    # the code has been modified to use these.        [mlh 2001-12-10]
+
+    def _get_contents(self):
+        return self.windows()
+
+    def add(self, win):
+        self._add_window(win)
+
+    def remove(self, win):
+        self._remove_window(self, win)
 
     def _add_window(self, win):
         self._windows.append(win)
