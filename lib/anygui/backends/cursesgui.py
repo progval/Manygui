d8638b5c64ec2b80e61a6743b16c7626aef010b8
jknapka <jknapka>
2001-12-10 02:31:37 +0000

    Moved support files for textgui/cursesgui to a new subdir.

--- lib/anygui/backends/cursesgui.py
+++ lib/anygui/backends/cursesgui.py
@@ -2,10 +2,10 @@
 from anygui.backends import *
 __all__ = anygui.__all__
 
-import scr_curses
+import anygui.backends.txtutils.scr_curses as scr_curses
 scr_curses.scr_init()
-import txtsupport
-txtsupport._support = scr_curses
+import anygui.backends.txtutils.txtgui as txtgui
+txtgui._support = scr_curses
 x,y = scr_curses._xsize,scr_curses._ysize
-txtsupport._set_scale(x,y)
-from txtsupport import *
+txtgui._set_scale(x,y)
+from anygui.backends.txtutils.txtgui import *
--- lib/anygui/backends/textgui.py
+++ lib/anygui/backends/textgui.py
@@ -2,7 +2,7 @@
 from anygui.backends import *
 __all__ = anygui.__all__
 
-import anygui.backends.scr_text as scr_text
-import txtsupport
-txtsupport._support = scr_text
-from txtsupport import *
+import anygui.backends.txtutils.scr_text as scr_text
+import anygui.backends.txtutils.txtgui as txtgui
+txtgui._support = scr_text
+from anygui.backends.txtutils.txtgui import *
