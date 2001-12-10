f3b6b1cafaf241b29677a65a20c30c76567aaf29
mlh <mlh>
2001-12-10 20:27:43 +0000

    *** empty log message ***

--- test/test_invisible.py
+++ test/test_invisible.py
@@ -3,14 +3,14 @@ from anygui import *
 app = Application()
 
 w1 = Window(title='', size=(200,100), visible=0)
-app.add(win)
+app.add(w1)
 
 lab = Label(text='Hello, world!', position=(30, 30))
 
 w1.add(lab)
 
 w2 = Window(title='This Window is visible', size=(200, 100))
-app.add(win)
+app.add(w2)
 
 def show(**kw):
     w1.visible = 1
