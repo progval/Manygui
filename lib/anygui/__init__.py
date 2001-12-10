9e033f7c09e6995064e76dcf9b25d5866972e888
mlh <mlh>
2001-12-10 00:40:09 +0000

    *** empty log message ***

--- lib/anygui/__init__.py
+++ lib/anygui/__init__.py
@@ -1,4 +1,4 @@
-_backends = 'msw gtk x java wx tk beos qt'
+_backends = 'msw gtk x java wx tk beos qt curses text'
 
 # Try to get the environment variables ANYGUI_WISHLIST (used
 # before anygui.wishlist), and ANYGUI_DEBUG (to print out
