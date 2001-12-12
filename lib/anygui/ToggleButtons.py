5e5ebf9a544fbd86d131f38ed43e9d038dfb696b
aleax <aleax>
2001-12-12 16:58:25 +0000

    removed now-redundant import

--- lib/anygui/ToggleButtons.py
+++ lib/anygui/ToggleButtons.py
@@ -1,6 +1,5 @@
 from anygui.Exceptions import UnimplementedMethod
 from anygui.GenericButtons import AbstractGenericButton
-from anygui.Models import BooleanModel
 
 class AbstractToggleButton(AbstractGenericButton):
 
