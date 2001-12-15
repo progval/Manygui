5ebe1877a331631866b0686b30b17a50841a5137
mlh <mlh>
2001-12-15 18:16:21 +0000

    *** empty log message ***

--- test/test_checkbox.py
+++ test/test_checkbox.py
@@ -7,7 +7,7 @@ def report(**kw):
 
 win = Window(width = 140, height = 90)
 app.add(win)
-box = CheckBox(x = 30, y = 30, width = 80, height = 30, 
+box = CheckBox(x = 30, y = 30, width = 100, height = 30, 
 	       text = "Check Box", on=1)
 link(box, report)
 
