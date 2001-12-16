d9a5e6c15273033edb4ce58b76ec8851924bc7fa
jknapka <jknapka>
2001-12-16 22:43:35 +0000

    Fixed textgui import problem.

--- lib/anygui/backends/textgui.py
+++ lib/anygui/backends/textgui.py
@@ -4,5 +4,9 @@ __all__ = anygui.__all__
 
 import anygui.backends.txtutils.scr_text as scr_text
 import anygui.backends.txtutils.txtgui as txtgui
-txtgui._support = scr_text
-from anygui.backends.txtutils.txtgui import *
+txtgui._scr = scr_text
+x,y = scr_text._xsize,scr_text._ysize
+txtgui._set_scale(x,y)
+for key in __all__:
+    globals()[key] = txtgui.__dict__[key]
+globals()['Canvas'] = txtgui.__dict__['Canvas']
