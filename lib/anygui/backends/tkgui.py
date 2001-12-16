4550fcd643636500001d3f615a97befcbb2b5c92
aleax <aleax>
2001-12-16 23:40:09 +0000

    Fixed some model-updating issues, general cleanup, canvas colors now work

--- lib/anygui/backends/tkgui.py
+++ lib/anygui/backends/tkgui.py
@@ -1,15 +1,12 @@
-
 from anygui.backends import *
 __all__ = anygui.__all__
-
-################################################################
 
 from Tkinter import * # A bit risky
 import re, Tkinter, os
 
 # Figure out proper setting for Tk's "exportselection" option.
-EXPORTSELECTION='true'
-if os.name in ['nt','os2']:
+EXPORTSELECTION = 'true'
+if os.name in ['nt', 'os2']:
     EXPORTSELECTION = 'false'
 
 class ComponentMixin:
@@ -29,63 +26,57 @@ class ComponentMixin:
         return self._tk_comp is not None
 
     def _ensure_created(self):
-        if self._tk_comp is None:
-            if self._container is not None:
-                parent = self._container._tk_comp
-            else:
-                parent = None
-            frame = self._tk_class(parent,**self._tk_opts
-                                   #text=self._get_tk_text(),
-                                   #style=self._tk_style
-                                   )
-            # FIXME: Should be handled by _ensure_title and _ensure_text
-            if self._tk_class is not Toplevel: #?
-                try:
-                    frame.config(text=self._get_tk_text())
-                except: # FIXME: Ugly... (ListBox etc. doesn't have a text property)
-                    pass
-                if self._tk_class is Tkinter.Label:
-                    frame.config(justify=self._tk_style, anchor=W) # FIXME: anchor assumes LEFT
-            else:
-                frame.title(self._get_tk_text())
-            self._tk_comp = frame
+        if self._is_created(): return 0
 
-            # Handle exportselection.
+        if self._container is None: parent = None
+        else: parent = self._container._tk_comp
+
+        component = self._tk_class(parent, **self._tk_opts)
+
+        # FIXME: Should be handled by _ensure_title and _ensure_text
+        if self._tk_class is Toplevel: #?
+            component.title(self._get_tk_text())
+        else:
             try:
-                self._tk_comp.configure(exportselection=EXPORTSELECTION)
-            except:
-                pass            
+                component.config(text=self._get_tk_text())
+            except: # not all widgets have a 'text' property
+                pass
+            if self._tk_class is Tkinter.Label:
+                component.config(justify=self._tk_style, anchor=W)
 
-            return 1
-        return 0
+        try:
+            component.configure(exportselection=EXPORTSELECTION)
+        except:
+            pass            
+
+        self._tk_comp = component
+
+        return 1
 
     def _ensure_events(self):
         pass
 
+    def _show(self):
+        self._tk_comp.place(x=self._x, y=self._y,
+                            width=self._width, height=self._height)
+
+    def _hide(self):
+        self._tk_comp.place_forget()
+
     def _ensure_geometry(self):
-        if self._tk_comp and self._visible:
-            # FIXME: Does this work with windows?
-            self._tk_comp.place(x=self._x, y=self._y,
-                                width=self._width, height=self._height)
+        if self._tk_comp and self._visible: self._show()
 
     def _ensure_visibility(self):
         if self._tk_comp:
-            # FIXME: Hack...
-            if self._visible:
-                self._ensure_geometry()
-            else:
-                self._tk_comp.place_forget()
+            if self._visible: self._show()
+            else: self._hide()
 
     def _ensure_enabled_state(self):
         if self._tk_comp:
-            # FIXME: Ugly solution
-            try:
-                if self._enabled:
-                    self._tk_comp.config(state=NORMAL)
-                else:
-                    self._tk_comp.config(state=DISABLED)
-            except TclError:
-                pass # Widget didn't support -state
+            if self._enabled: newstate = NORMAL
+            else: newstate = DISABLED
+            try: self._tk_comp.config(state=newstate)
+            except TclError: pass # Widget doesn't support -state
 
     def _ensure_destroyed(self):
         if self._tk_comp:
@@ -93,10 +84,8 @@ class ComponentMixin:
             self._tk_comp = None
 
     def _get_tk_text(self):
-        # helper function for creation
-        # returns the text required for creation.
-        # This may be the _text property, or _title, ...,
-        # depending on the subclass
+        # helper function for creation: return text needed for creation
+        # (normally _text, maybe _title or other depending on the class)
         return self._text
 
     def _ensure_text(self):
@@ -128,8 +117,8 @@ class Canvas(ComponentMixin, AbstractCanvas):
 
     def _call_deferred_methods(self):
         for methcall in self._deferred_methods:
-            meth = getattr(self,methcall[0])
-            apply(meth,methcall[1:])
+            meth = getattr(self, methcall[0])
+            apply(meth, methcall[1:])
 
     def clear(self):
         if self._tk_comp:
@@ -162,26 +151,19 @@ class Canvas(ComponentMixin, AbstractCanvas):
                                                 width=edgeWidth,
                                                 outline=edgeColor)
         else:
-            if fillColor == '':
-                d = {'fill': edgeColor, 'width': edgeWidth}
-                item = apply(self._tk_comp.create_line, pointlist, d)
-                self._items.append(item)
-            else:
+            if fillColor:
                 item = self._tk_comp.create_polygon(pointlist,
                                                     fill=fillColor,
                                                     outline='')
                 self._items.append(item)
-                d = {'fill': edgeColor, 'width': edgeWidth}
-                item = apply(self._tk_comp.create_line, pointlist, d)
-                self._items.append(item)
+            d = {'fill': edgeColor, 'width': edgeWidth}
+            item = self._tk_comp.create_line(*pointlist, **d)
+            self._items.append(item)
 
 def _convert_color(c):
-    if c is None:
-        if c is Colors.transparent:
-            return ''
-        else:
-            return '#%02X%02X%02X' % \
-                   (int(c.red*255), int(c.green*255), int(c.blue*255))
+    if c is None or c is Colors.transparent: return ''
+    return '#%02X%02X%02X' % \
+           (int(c.red*255), int(c.green*255), int(c.blue*255))
 
 ################################################################
 
@@ -203,6 +185,9 @@ class ScrollableListBox(Tkinter.Frame):
 
     # Replacement for Tkinter.Listbox
 
+    _delegated_methods = """get configure bind curselection delete insert
+    select_clear select_set""".split()
+
     def __init__(self, *args, **kw):
         Tkinter.Frame.__init__(self, *args, **kw)
 
@@ -215,29 +200,8 @@ class ScrollableListBox(Tkinter.Frame):
         self._listbox.pack(side=LEFT, expand=YES, fill=BOTH)
         self._yscrollbar.config(command=self._listbox.yview)
 
-    def get(self, start, end):
-        return self._listbox.get(start, end)
-
-    def curselection(self):
-        return self._listbox.curselection()
-
-    def delete(self, start, end):
-        self._listbox.delete(start, end)
-
-    def insert(self, index, item):
-        self._listbox.insert(index, item)
-
-    def select_clear(self, selection):
-        self._listbox.select_clear(selection)
-
-    def select_set(self, selection):
-        self._listbox.select_set(selection)
-
-    def bind(self, event, callback):
-        self._listbox.bind(event, callback)
-
-    def configure(self,*args,**kw):
-        self._listbox.configure(*args,**kw)
+        for delegate in self._delegated_methods:
+            setattr(self, delegate, getattr(self._listbox, delegate))
 
 class ListBox(ComponentMixin, AbstractListBox):
     _tk_class = ScrollableListBox
@@ -264,10 +228,9 @@ class ListBox(ComponentMixin, AbstractListBox):
     def _ensure_events(self):
         if self._tk_comp:
             self._tk_comp.bind('<ButtonRelease-1>', self._tk_clicked)
-            self._tk_comp.bind('<KeyRelease-space>',self._tk_clicked)
+            self._tk_comp.bind('<KeyRelease-space>', self._tk_clicked)
 
     def _tk_clicked(self, event):
-        #self.do_action()
         send(self, 'select')
 
 ################################################################
@@ -280,12 +243,11 @@ class Button(ComponentMixin, AbstractButton):
             self._tk_comp.config(command=self._tk_clicked)
 
     def _tk_clicked(self):
-        #self.do_action()
         send(self, 'click')
 
     def _ensure_text(self):
         if self._tk_comp:
-            self._tk_comp.configure(text = self._text)
+            self._tk_comp.configure(text=self._text)
 
 class ToggleButtonMixin(ComponentMixin):
 
@@ -294,23 +256,24 @@ class ToggleButtonMixin(ComponentMixin):
             self._var.set(self.on)
 
     def _ensure_created(self):
-        ComponentMixin._ensure_created(self)
-        self._var = IntVar()
-        self._tk_comp.config(variable=self._var, anchor=W)
-        return 1
+        result = ComponentMixin._ensure_created(self)
+        if result:
+            self._var = IntVar()
+            self._tk_comp.config(variable=self._var, anchor=W)
+        return result
 
     def _ensure_events(self):
         self._tk_comp.config(command=self._tk_clicked)
 
     def _ensure_text(self):
         if self._tk_comp:
-            self._tk_comp.configure(text = self._text)
+            self._tk_comp.configure(text=self._text)
 
 class CheckBox(ToggleButtonMixin, AbstractCheckBox):
     _tk_class = Checkbutton
 
     def _tk_clicked(self):
-        self.on = not self.on # FIXME: ??
+        self.modify(on=not self.on)
         send(self, 'click')
 
 class RadioButton(ToggleButtonMixin, AbstractRadioButton):
@@ -318,12 +281,13 @@ class RadioButton(ToggleButtonMixin, AbstractRadioButton):
 
     def _tk_clicked(self):
         if self.group is not None:
-            self.group.value = self.value
+            self.group.modify(value=self.value)
         send(self, 'click')
 
     def _ensure_created(self):
         result = ToggleButtonMixin._ensure_created(self)
-        self._tk_comp.config(value=1) # FIXME: Shaky...
+        if result:
+            self._tk_comp.config(value=1) # FIXME: Shaky...
         return result
 
 ################################################################
@@ -340,39 +304,39 @@ class DisabledTextBindings:
         self._ctl = 0
         self._alt = 0
         self._shift = 0
-        self._tk_comp.bind("<Key>",self._keybinding)
-        self._tk_comp.bind("<KeyPress-Control_L>",self._ctldown)
-        self._tk_comp.bind("<KeyRelease-Control_L>",self._ctlup)
-        self._tk_comp.bind("<KeyPress-Alt_L>",self._altdown)
-        self._tk_comp.bind("<KeyRelease-Alt_L>",self._altup)
-        self._tk_comp.bind("<KeyPress-Shift_L>",self._shiftdown)
-        self._tk_comp.bind("<KeyRelease-Shift_L>",self._shiftup)
-        self._tk_comp.bind("<Key-Insert>",self._insertbinding)
-        self._tk_comp.bind("<Key-Up>",self._arrowbinding)
-        self._tk_comp.bind("<Key-Down>",self._arrowbinding)
-        self._tk_comp.bind("<Key-Left>",self._arrowbinding)
-        self._tk_comp.bind("<Key-Right>",self._arrowbinding)
-        self._tk_comp.bind("<ButtonRelease>",self._insertbinding)
+        self._tk_comp.bind("<Key>", self._keybinding)
+        self._tk_comp.bind("<KeyPress-Control_L>", self._ctldown)
+        self._tk_comp.bind("<KeyRelease-Control_L>", self._ctlup)
+        self._tk_comp.bind("<KeyPress-Alt_L>", self._altdown)
+        self._tk_comp.bind("<KeyRelease-Alt_L>", self._altup)
+        self._tk_comp.bind("<KeyPress-Shift_L>", self._shiftdown)
+        self._tk_comp.bind("<KeyRelease-Shift_L>", self._shiftup)
+        self._tk_comp.bind("<Key-Insert>", self._insertbinding)
+        self._tk_comp.bind("<Key-Up>", self._arrowbinding)
+        self._tk_comp.bind("<Key-Down>", self._arrowbinding)
+        self._tk_comp.bind("<Key-Left>", self._arrowbinding)
+        self._tk_comp.bind("<Key-Right>", self._arrowbinding)
+        self._tk_comp.bind("<ButtonRelease>", self._insertbinding)
 
         # Easy place to put this - not _editable-related, but common
-        # to text widgets.
-        self._tk_comp.bind("<Leave>",self._update_model)
+        # to all text widgets.
+        self._tk_comp.bind("<Leave>", self._update_model)
 
     # Track modifier key state.
-    def _ctldown(self,ev):
+    def _ctldown(self, ev):
         self._ctl = 1
-    def _ctlup(self,ev):
+    def _ctlup(self, ev):
         self._ctl = 0
-    def _altdown(self,ev):
+    def _altdown(self, ev):
         self._alt = 1
-    def _altup(self,ev):
+    def _altup(self, ev):
         self._alt = 0
-    def _shiftdown(self,ev):
+    def _shiftdown(self, ev):
         self._shift = 1
-    def _shiftup(self,ev):
+    def _shiftup(self, ev):
         self._shift = 0
 
-    def _keybinding(self,ev):
+    def _keybinding(self, ev):
         """ This method binds all keys, and causes them to be
         ignored when _editable is not set. """
         if self._editable:
@@ -381,7 +345,7 @@ class DisabledTextBindings:
             # This is truly horrid. Please add appropriate
             # code for Mac platform, someone.
             if (ev.char == "\x03") or (ev.char == "c" and self._alt):
-                # DON"T ignore this key: it's a copy operation.
+                # DON'T ignore this key: it's a copy operation.
                 return None
             return "break"
 
@@ -435,7 +399,7 @@ class TextField(ComponentMixin, AbstractTextField, DisabledTextBindings):
             start, end = self._selection
             self._tk_comp.selection_range(start, end)
 
-    def _do_ensure_selection(self,ev=None):
+    def _do_ensure_selection(self, ev=None):
         self._ensure_selection()
 
     def _ensure_editable(self):
@@ -448,17 +412,17 @@ class TextField(ComponentMixin, AbstractTextField, DisabledTextBindings):
         if self._tk_comp:
             self._tk_comp.bind('<KeyPress-Return>', self._send_action) # do_action
             if EXPORTSELECTION == 'false':
-                self._tk_comp.bind('<KeyRelease-Tab>',self._do_ensure_selection)
+                self._tk_comp.bind('<KeyRelease-Tab>', self._do_ensure_selection)
 
-    def _update_model(self,ev):
-        # self.model.value = self._tk_comp.get()
-        # AM20011209: changed, less-strong coupling to front-end
-        #     implementation strategies
-        self._get_text()
+    def _update_model(self, ev):
+        self.modify(text=self._backend_text())
 
 class ScrollableTextArea(Tkinter.Frame):
 
-    # Replacemenent for Tkinter.Text
+    # Replacement for Tkinter.Text
+
+    _delegated_methods = """bind config get mark_names index delete insert
+    mark_set tag_add tag_remove tag_names configure""".split()
 
     def __init__(self, *args, **kw):
         Tkinter.Frame.__init__(self, *args, **kw)
@@ -477,48 +441,16 @@ class ScrollableTextArea(Tkinter.Frame):
         self._yscrollbar.config(command=self._textarea.yview)
         self._xscrollbar.config(command=self._textarea.xview)
 
-    def bind(self,*args,**kw):
-        self._textarea.bind(*args,**kw)
-
-    def config(self, **kw):
-        self._textarea.config(**kw)
-
-    def get(self, start, end):
-        return self._textarea.get(start, end)
-
-    def mark_names(self):
-        return self._textarea.mark_names()
-
-    def index(self, mark):
-        return self._textarea.index(mark)
-
-    def delete(self, start, end):
-        self._textarea.delete(start, end)
-
-    def insert(self, index, text):
-        self._textarea.insert(index, text)
-
-    def mark_set(self, mark, position):
-        self._textarea.mark_set(mark, position)
-
-    def tag_add(self, tag, start, end):
-        self._textarea.tag_add(tag, start, end)
-
-    def tag_remove(self, tag, start, end):
-        self._textarea.tag_remove(tag, start, end)
-
-    def tag_names(self): return self._textarea.tag_names()
-
-    def configure(self,*args,**kw):
-        self._textarea.configure(*args,**kw)
+        for delegate in self._delegated_methods:
+            setattr(self, delegate, getattr(self._textarea, delegate))
 
 class TextArea(ComponentMixin, AbstractTextArea, DisabledTextBindings):
-    _tk_class = ScrollableTextArea # Tkinter.Text
+    _tk_class = ScrollableTextArea
 
     def _ensure_created(self):
         result = ComponentMixin._ensure_created(self)
         if result:
-            self._tk_comp.config(wrap=NONE) #, wrap=WORD)
+            self._tk_comp.config(wrap=NONE)
             self._install_bindings()
         return result
 
@@ -526,16 +458,16 @@ class TextArea(ComponentMixin, AbstractTextArea, DisabledTextBindings):
         if self._tk_comp:
             return self._tk_comp.get(1.0, END)[:-1] # Remove the extra newline. (Always?)
 
-    def _to_char_index(self,idx):
+    def _to_char_index(self, idx):
         # This is no fun, but there doesn't seem to be an easier way than
         # counting the characters in each line :-(   -- jak
         txt = self._tk_comp
         idx = txt.index(idx)
-        line,col = idx.split(".")
-        line=int(line)
+        line, col = idx.split(".")
+        line = int(line)
         tlen = 0
-        for ll in range(1,line):
-            tlen += len(txt.get("%s.0"%ll,"%s.end"%ll))
+        for ll in range(1, line):
+            tlen += len(txt.get("%s.0"%ll, "%s.end"%ll))
             tlen += 1
         tlen += int(col)
         return tlen
@@ -558,12 +490,12 @@ class TextArea(ComponentMixin, AbstractTextArea, DisabledTextBindings):
                 self._tk_comp.config(state=NORMAL) # Make sure we can change the text
                 self._tk_comp.delete(1.0, END)
                 self._tk_comp.insert(1.0, self._text)
-                self._ensure_editable() # Make the state is synched
+                self._ensure_editable() # Make sure the state is sync'ed
 
     def _ensure_selection(self):
         if self._tk_comp:
             start, end = self._selection
-            self._tk_comp.tag_remove('sel','1.0','end')
+            self._tk_comp.tag_remove('sel', '1.0', 'end')
             self._tk_comp.tag_add('sel', '1.0 + %s char' % start, '1.0 + %s char' % end)
 
     def _ensure_editable(self):
@@ -571,7 +503,7 @@ class TextArea(ComponentMixin, AbstractTextArea, DisabledTextBindings):
         pass
 
     def _update_model(self,ev):
-        self.text = self._tk_comp.get("1.0","end")
+        self.modify(text=self._tk_comp.get("1.0","end"))
 
 ################################################################
 
