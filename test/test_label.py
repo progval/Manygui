9a03cc3c2b1c89dd4b15fb0a0ca4c96d64e9369d
jknapka <jknapka>
2001-12-16 17:06:11 +0000

    Labels no longer overlap.

--- test/test_label.py
+++ test/test_label.py
@@ -4,8 +4,8 @@ app = Application()
 
 win = Window(width = 160, height = 100)
 app.add(win)
-lb1 = Label(x = 30, y = 30, height = 32, text = "Spam!")
-lb2 = Label(x = 30, y = 48, height = 32, text = "Glorious Spam!")
+lb1 = Label(x = 30, y = 18, height = 32, text = "Spam!")
+lb2 = Label(x = 30, y = 58, height = 32, text = "Glorious Spam!")
 
 win.add(lb1)
 win.add(lb2)
