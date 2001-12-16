1e0007d75d7c7dde77598bbca6ac9932750f9a01
aleax <aleax>
2001-12-16 19:31:41 +0000

    Added modattr and modify methods to class Attrib

--- lib/anygui/Mixins.py
+++ lib/anygui/Mixins.py
@@ -13,6 +13,7 @@ weakref = None
 _no_auto_ensures = '_ensure_created _ensure_destroyed _ensure_events'.split()
 
 # _ensure_* methods that must be called no more than once per object
+# (currently, this facility is not being used)
 _ensures_once = ()
 
 # pairwise order constraints on _ensure_* method calls
@@ -74,11 +75,13 @@ def topological_sort(items, constraints):
     return result
 
 class Attrib:
-    """Attrib: mix-in class to support attribute getting & setting
+    """Attrib: mix-in class to support attribute getting & setting.
+
 
     Each attribute name may have a setter method _set_name and/or a getter
     method _get_name.  If only the latter, it's a read-only attribute.  If
-    no _set_name, attribute is set directly in self.__dict__.
+    no _set_name, attribute is set directly in self.__dict__.  This only
+    applies to attribute-names that do NOT start with '_'.
 
     If the value being set exposes a method .assigned, it's called just
     after the attribute assignment; if the previously-set value exposes
@@ -90,12 +93,26 @@ class Attrib:
     and options, and an __init__ with similar functionality.  __init__
     also handles attributes listed in self.explicit_attributes.
 
+    Another, similar feature of class Attrib: it supplies a modattr method
+    that tries to change an attribute's value in-place, if feasible, rather
+    than re-bind it.  Changing in-place means trying _modify_name (rather
+    than _set_name), assigning to self.name[:], and assigning to
+    self.name.value.  In the end, self.name is re-bound if in-place
+    modification fails.  If the value being changed exposes a method
+    .modified, it's called just after an in-place modification succeeds.
+
+    Method modify has the same interface as set (to modify potentially
+    more than one attribute at once) but uses modattr rather than setattr.
+
+
     Attrib also supplies a default update method, which calls all the
     relevant methods named _ensure_* if flag _inhibit_update is false.
     In this release, all _ensure_* are called; eventually, some kind
     of mechanism will use the hints to be more selective/optimizing.
     Attrib's responsibilities include calling certain _ensure_* methods
-    only once, and enforcing a calling order among _ensure_* methods.
+    only once, enforcing a calling order among _ensure_* methods, and
+    avoiding auto-calls of _ensure_* methods in Mixins._no_auto_ensures.
+
 
     Note that Attrib embodies two patterns (attribute setting/getting
     and update functionality) and is thus "Alexandrian dense"; cfr
@@ -103,7 +120,7 @@ class Attrib:
     such "dense" approaches and the resulting "profound" code.
     """
 
-    _all_ensures = []
+    _all_ensures = []           # no _ensure_* called until Attrib.__init__
     _inhibit_update = 0         # default Attribs are always update-enabled
 
     def __setattr__(self, name, value):
@@ -114,12 +131,12 @@ class Attrib:
                 if hasattr(self, "_get_"+name):
                     raise SetAttributeError(self, name)
             else:
-                try: getattr(self.name).removed(self, name)
+                try: getattr(self, name).removed(self, name)
                 except: pass
-                setter(value)
+                inhibit_update = setter(value)
                 try: value.assigned(self, name)
                 except: pass
-                self.update(name=name)
+                if not inhibit_update: self.update(name=name)
                 return
         self.__dict__[name] = value
         if name[0]!='_':
@@ -135,13 +152,49 @@ class Attrib:
         else:
             return getter()
 
-    def set(self, *args, **kwds):
+    def _set_or_mod(self, func, *args, **kwds):
         for opt in args:
             # kwds.update(opt.__dict__) # Doesn't work in Jython 2.1a1
             for key, val in opt.__dict__.items():
                 kwds[key] = val
         for name, value in kwds.items():
-            setattr(self, name, value)
+            func(self, name, value)
+
+    def set(self, *args, **kwds):
+        return self._set_or_mod(setattr, *args, **kwds)
+
+    def modify(self, *args, **kwds):
+        return self._set_or_mod(self.__class__.modattr, *args, **kwds)
+
+    def modattr(self, name, value):
+        if name[0]!='_':
+            try: modifier = getattr(self, '_modify_'+name)
+            except AttributeError: pass
+            else:
+                # found a modifier-method, delegate the task to it
+                inhibit_update = modifier(value)
+                try: getattr(self, name).modified()
+                except: pass
+                if not inhibit_update: self.update(modified=name)
+                return
+
+        # we need to perform the modification-task directly
+        old_value = getattr(self, name, None)
+        # try assigning to the "all-object slice"
+        try: old_value[:] = value
+        except:
+            # try assigning to the "value" attribute of the old-value
+            try: old_value.value = value
+            except:
+                # no in-place mod, so, just set it (bind or re-bind)
+                setattr(self, name, value)
+                return
+        # in-place modification has succeeded, alert the old_value (if
+        # it supplies a suitable method) and any watchers of 'self'
+        try: old_value.modified()
+        except: pass
+        if name[0]!='_' and not inhibit_update:
+            self.update(modified=name)
 
     def __init__(self, *args, **kwds):
         # _all_ensures must be computed exactly once per concrete class
