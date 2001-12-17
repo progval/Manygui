8bde81c6a7937f6e0db592fb3776c89edcd365c6
jknapka <jknapka>
2001-12-17 03:35:02 +0000

    Fixed bad import logic.

--- lib/anygui/backends/cursesgui.py
+++ lib/anygui/backends/cursesgui.py
@@ -5,7 +5,8 @@ __all__ = anygui.__all__
 import anygui.backends.txtutils.scr_curses as scr_curses
 scr_curses.scr_init()
 import anygui.backends.txtutils.txtgui as txtgui
-txtgui._support = scr_curses
+txtgui._scr = scr_curses
 x,y = scr_curses._xsize,scr_curses._ysize
+scr_curses.dbg("x,y=",x,y)
 txtgui._set_scale(x,y)
 from anygui.backends.txtutils.txtgui import *
