4ced4ba1ff4cb6688c50f3d0937e0845036e4233
jknapka <jknapka>
2001-12-19 07:15:38 +0000

    Added log facility. Tests should now import "log" from anygui.Utils and use "log(x,y,z)" instead of "print x,y,z".

--- lib/anygui/Utils.py
+++ lib/anygui/Utils.py
@@ -20,3 +20,33 @@ class Bunch:
         self.__dict__.update(kwds)
 
 class Options(Bunch): pass
+
+import sys
+class Log:
+
+    def __init__(self,fileobj=None):
+        if fileobj is None:
+            fileobj = sys.stderr
+        self._f = fileobj
+
+    def log(self,*items):
+        for i in items:
+            self._f.write(str(i))
+            self._f.write(' ')
+        self._f.write('\n')
+
+    def setLogFile(self,fileobj):
+        if type(fileobj) == type(""):
+            self._f = open(fileobj,"w")
+        else:
+            self._f = fileobj
+
+_logger = Log()
+
+def log(*items):
+  _logger.log(*items)
+
+def setLogFile(fileobj):
+    _logger.setLogFile(fileobj)
+    
+    
