15d39583042e843a07fea82e6d2a4547fbcd52c4
jknapka <jknapka>
2002-07-28 23:15:16 +0000

    Test case for the menu subsystem.

---
+++ test/test_menu.py
@@ -0,0 +1,67 @@
+from anygui import Application, Window, Menu, Label, link
+
+app = Application()
+
+win = Window(geometry=(50,50,100,100))
+
+# A menu to choose skits.
+skitMenu = Menu(text="Skit")
+win.add(skitMenu)
+
+# Populate the skit menu. The objects returned by the
+# add...() calls are handles that permit us to operate
+# on the menu items later.
+dedPrt = skitMenu.addCommand(text="Dead Parrot")
+pengTv = skitMenu.addCommand(text="Penguin on the Telly")
+lumbrJk = skitMenu.addCommand(text="Lumberjack")
+
+skitMenu.addSeparator()
+
+# Enable or disable skit silliness: a checkable menu item.
+enSilly = skitMenu.addCheck(text="Enable Silliness",on=0)
+def enableSilly(ev):
+    # Enable or disable the silliness selection based
+    # on the value of the enSilly menu item.
+    if enSilly.on:
+        # subMenu is defined below.
+        subMenu.enabled=1
+    else:
+        subMenu.enabled=0
+link(enSilly,enableSilly)
+
+# A sub-menu to allow silliness level to be selected:
+# a mutually-exclusive set of checkable items (radio
+# items?)
+subMenu = Menu(text="Silliness...",enabled=0)
+silliness = []
+def handleSillySelect(ev):
+    # Enforce mutual exclusion of silliness level.
+    for item in silliness:
+        if item != ev.source:
+            item.on=0
+        else:
+            item.on = 1
+for txt in ["Slight","Moderate","Unbearable"]:
+    silliness.append(subMenu.addCheck(text=txt))
+    link(silliness[-1],handleSillySelect)
+skitMenu.addMenu(subMenu)
+
+# And a menu for help...
+helpMenu = Menu(text="Help")
+helpMenu.addCommand(text="About Anygui")
+win.add(helpMenu)
+
+lbl = Label(text="No skit selected")
+win.add(lbl)
+
+def chooseSkit(ev):
+    # Handle a skit choice. A menu event should have a
+    # 'text' property containing the text of the menu item.
+    lbl.text = ev['text']
+
+link(dedPrt,chooseSkit)
+link(pengTv,chooseSkit)
+link(lumbrJk,chooseSkit)
+
+app.add(win)
+app.run()
