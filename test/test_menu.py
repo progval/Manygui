55748a12848aff6a780f2b57bc3607b4ff8f2f6a
jknapka <jknapka>
2002-08-06 16:29:46 +0000

    Changed to show menu.contents = menuitems protocol.

--- test/test_menu.py
+++ test/test_menu.py
@@ -1,4 +1,4 @@
-from anygui import Application, Window, Menu, Label, link, TextField
+from anygui import Application, Window, Menu, MenuCheck, Label, link, TextField
 from anygui.LayoutManagers import SimpleGridManager
 
 app = Application()
@@ -14,17 +14,31 @@ win.addMenu(mbar)
 skitMenu = Menu(text="Skit")
 mbar.add(skitMenu)
 
-# Populate the skit menu. The objects returned by the
-# add...() calls are handles that permit us to operate
-# on the menu items later.
+# Populate the skit menu.
+lbl = Label(text="No skit selected")
+win.add(lbl)
+def chooseSkit(ev):
+    # Handle a skit choice. A menu event should have a
+    # 'text' property containing the text of the menu item.
+    lbl.text = ev.text
+
+# The objects returned by the add...() calls are handles that permit
+# us to operate on the menu items later.
 dedPrt = skitMenu.addCommand(text="Dead Parrot")
 pengTv = skitMenu.addCommand(text="Penguin on the Telly")
-lumbrJk = skitMenu.addCommand(text="Lumberjack")
+link(dedPrt,chooseSkit)
+link(pengTv,chooseSkit)
+
+# We can also supply a command to the command adder, which
+# does an implicit link() for us. For menu structures
+# that don't change much, this lets us build menus
+# without having to deal with every menu item proxy.
+# This works for checkboxes as well.
+lumbrJk = skitMenu.addCommand(text="Lumberjack",command=chooseSkit)
 
 skitMenu.addSeparator()
 
 # Enable or disable skit silliness: a checkable menu item.
-enSilly = skitMenu.addCheck(text="Enable Silliness",on=0)
 def enableSilly(ev):
     # Enable or disable the silliness selection based
     # on the value of the enSilly menu item.
@@ -33,7 +47,7 @@ def enableSilly(ev):
         subMenu.enabled=1
     else:
         subMenu.enabled=0
-link(enSilly,enableSilly)
+enSilly = skitMenu.addCheck(text="Enable Silliness",on=0,command=enableSilly)
 
 # A sub-menu to allow silliness level to be selected:
 # a mutually-exclusive set of checkable items (radio
@@ -47,36 +61,26 @@ def handleSillySelect(ev):
             item.on=0
         else:
             item.on = 1
+
+# We can set the contents of a menu in one go, if we want,
+# instead of calling addThingy() a bunch of times.
 for txt in ["Slight","Moderate","Unbearable"]:
-    silliness.append(subMenu.addCheck(text=txt))
-    link(silliness[-1],handleSillySelect)
+    silliness.append(MenuCheck(text=txt,command=handleSillySelect))
+subMenu.contents = silliness
 skitMenu.add(subMenu)
 
 skitMenu.addSeparator()
-addCmd = skitMenu.addCommand(text="Add Skit")
 def addSkit(*args,**kws):
-    cmd = skitMenu.addCommand(text=entry.text,index=0)
-    link(cmd,chooseSkit)
-link(addCmd,addSkit)
+    cmd = skitMenu.addCommand(text=entry.text,index=0,command=chooseSkit)
+addCmd = skitMenu.addCommand(text="Add Skit",command=addSkit)
 
 # And a menu for help...
 helpMenu = Menu(text="Help")
 helpMenu.addCommand(text="About Anygui")
 mbar.add(helpMenu)
 
-lbl = Label(text="No skit selected")
-win.add(lbl)
 entry = TextField(text="Enter new skit",selection=(0,15))
 win.add(entry)
-
-def chooseSkit(ev):
-    # Handle a skit choice. A menu event should have a
-    # 'text' property containing the text of the menu item.
-    lbl.text = ev.text
-
-link(dedPrt,chooseSkit)
-link(pengTv,chooseSkit)
-link(lumbrJk,chooseSkit)
 
 app.add(win)
 app.run()
